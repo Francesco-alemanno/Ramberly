8698500e998318f33962a69318c9d274e41b09f1
Francesco <alemannof43@gmail.com>
2025-02-26 15:38:56 +0100

    inizializzazione tabelle

--- back-end-ramberly/src/server/initDB.js
+++ back-end-ramberly/src/server/initDB.js
@@ -4,9 +4,54 @@ import dotenv from "dotenv";
 dotenv.config();
 const url = process.env.URL; // da inizializzare
 
-const db = pgPromise()("postgres://<USERNAME>:<PASSWORD>@localhost:5432/<DATABASE>");
+const db = pgPromise()("postgres//postgresFingerskate1@localhost5432/Ramberly");
 const setupDb = async () => {
-  await db.none(
-    ``)
+  try {
+    await db.none(`DROP TABLE IF EXISTS users`);
+    await db.none(`DROP TABLE IF EXISTS eventi `);
+
+    await db.none(`CREATE TABLE users (
+    id SERIAL PRIMARY KEY NOT NULL,
+    nome TEXT NOT NULL,
+    cognome TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    livello INTEGER DEFAULT (1),
+    img BLOB,
+    sesso CHAR(1) NOT NULL,
+  peso DECIMAL(5,2) NOT NULL,
+  attivita INTEGER NOT NULL,
+  monitoraggio BOOLEAN NOT NULL,
+  gruppo BOOLEAN NOT NULL,
+  sfide BOOLEAN NOT NULL,
+  running BOOLEAN,
+  escursione BOOLEAN,
+  biking BOOLEAN,
+  camminata BOOLEAN,
+  seguiti INTEGER[] DEFAULT ARRAY[],
+  seguaci INTEGER[] DEFAULT ARRAY[]
+
+    )`);
+
+    await db.none(`CREATE TABLE eventi (
+      id_evento SERIAL PRIMARY KEY NOT NULL,
+      id_creatore INTEGER NOT NULL,
+      FOREIGN KEY (id_creatore) REFERENCES users(id),
+      nome_evento TEXT NOT NULL,
+      start TEXT NOT NULL,
+      finish TEXT NOT NULL,
+      img BLOB NOT NULL,
+      distanza DECIMAL(10,2) NOT NULL,
+      orario TIME NOT NULL,
+      data DATE NOT NULL,
+      partecipanti INTEGER[] DEFAULT ARRAY[],
+      privacy  CHAR(1) NOT NULL,
+      difficolta CHAR(1) NOT NULL
+      ) `);
+
+    console.log("tabelle create correttamente");
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 setupDb();
