a3b74f06dcaa1b221fbfbc5ae984b50e4ce05c37
Francesco <alemannof43@gmail.com>
2025-03-02 16:08:00 +0100

    rimesso server giusto

--- back-end-ramberly/src/server/initDB.js
+++ back-end-ramberly/src/server/initDB.js
@@ -4,12 +4,12 @@ import bcrypt from 'bcrypt'
 dotenv.config();
 // const url = process.env.URL; // da inizializzare
 
-// export const db = pgPromise()(
-//   "postgresql://team_user:Ramberly31@130.25.236.251:5432/team_db?schema=public"
-// );
 export const db = pgPromise()(
-  "postgresql://postgres:Fingerskate1@localhost:5432/ramberly"
+  "postgresql://team_user:Ramberly31@130.25.236.251:5432/team_db?schema=public"
 );
+// export const db = pgPromise()(
+//   "postgresql://postgres:Fingerskate1@localhost:5432/ramberly"
+// );
 const setupDb = async () => {
   try {
     await db.none(`
